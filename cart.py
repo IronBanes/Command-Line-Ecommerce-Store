9d31332974d14830ad8791aff52fb613fa48c193
Caliberj456 <130528251+Caliberj456@users.noreply.github.com>
2023-04-23 15:21:01 -0500

    Update cart.py

---
+++ cart.py
@@ -0,0 +1,15 @@
+import mysql.connector
+import sys
+class inventory:
+    def _init_ (self):
+        self.itemID = ""
+        self.shippingAddress = ""
+        self.card = ""
+        self.ccv = ""
+        self.expiration = ""
+
+    
+
+
+
+
