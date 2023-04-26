2dae323a43765ab96facdea9a173fa700fc89017
Caliberj456 <130528251+Caliberj456@users.noreply.github.com>
2023-04-26 16:16:18 -0500

    Update inventory.py

---
+++ inventory.py
@@ -0,0 +1,24 @@
+import mysql.connector
+import sys
+
+class inventory:
+    def _init_ (self):
+        self.itemID = ""
+
+    def viewGameInventory():
+        storeloop = 0
+        while (storeloop != 0):
+            os.system("cls")
+
+            print("0. back")
+            print("1. View Games")
+            print("2. Cart")
+
+            if(storeloop == "0"):
+
+            elif(storeloop = "1"):
+
+            elif(storeloop = "2"):
+                print("Cart")
+
+
