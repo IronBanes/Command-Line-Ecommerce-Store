3f32f05fc46c66220cf635cfae2bd10efe8be265
IronBanes <gavinator0000@gmail.com>
2023-04-29 18:49:03 -0500

    update to user and main

--- main.py
+++ main.py
@@ -13,10 +13,91 @@ def manageaccount():
     while (1):
         #os.system("cls")
         print("0. Back to Main Menu")
-        print("1. ")
-        print("2. Cart")
-        print("3. Manage Account")
+        print("1. Change First Name")
+        print("2. Change Last Name")
+        print("3. Change Username")
+        print("4. Change Password")
+        print("5. Change Email")
+        print("6. Change Shipping Information")
+        print("7. Change Payment Information")
         manageacc = input("Select Menu Option: ")
+
+        if(manageacc == "0"):
+            print("Thank you for shopping with us")
+            print("Have a Good Day")
+            sys.exit()
+
+        elif(manageacc == "1"):#first name
+            print("The current first name is ",user.getfirstname())
+            newfirstname = input("What would you like to change it to: ")
+            user.setfirstname(newfirstname)
+            print("The First Name has been changed")
+
+        elif(manageacc == "2"):#last name
+            print("The current last name is ",user.getlastname())
+            newlastname = input("What would you like to change it to: ")
+            user.setlastname(newlastname)
+            print("The Last Name has been changed")
+
+        elif(manageacc == "3"):#username
+            print("The current Username is ",user.getusername())
+            newusername = input("What would you like to change it to: ")
+            user.setlastname(newusername)
+            print("The Username has been changed")
+
+        elif(manageacc == "4"):#password
+            oldpassword = input("Enter current password: ")
+            newpassword = input("Enter new password: ")
+            if(oldpassword == user.getpassword):
+                user.setpassword(newpassword)
+                print("Password has been changed")
+            else:
+                print("The password you enter does not match the current user password.")
+
+        elif(manageacc == "5"):#email
+            print("The current Email is ",user.getemail())
+            newemail = input("What would you like to change it to: ")
+            user.setemail(newemail)
+            print("The Email has been changed")
+
+        elif(manageacc == "6"):#shipping info
+            if(user.getpaymentinfo):
+                print("Your current Shipping information.")
+                print("Address: ",user.getaddress())
+                print("City: ",user.getcity())
+                print("State: ",user.getstate())
+                print("Zipcode: ",user.getzipcode())
+                print()
+                print("Please enter your new Shipping information")
+            else:
+                print("Please enter your Shipping information")
+
+            address = input("Address: ")
+            city = input("City: ")
+            state = input("State: ")
+            zipcode = input("Zipcode: ")
+            user.setshippinginfo(address, city, state, zipcode)
+            print("Shipping information has been changed.")
+
+
+        elif(manageacc == "7"):#payment info
+            if(user.getpaymentinfo):
+                print("Your current card information.")
+                print("Card:",user.getcard())
+                print("CVV:", user.getcvv())
+                print()
+                print("Please enter your new card information")
+            else:
+                print("Please enter your card information")
+
+            card = input("Card: ")
+            cvv = input("CVV: ")
+            user.setpaymentinfo(card, cvv)
+            print("Payment information has been changed.")
+            
+
+
+
 
 def store():
     while(1):
@@ -28,6 +109,8 @@ def store():
         mainstoreloop = input("Select Menu Option: ")
     
         if(mainstoreloop == "0"):
+            print("Thank you for shopping with us")
+            print("Have a Good Day")
             sys.exit()
         elif(mainstoreloop == "1"):
             print("store")
@@ -41,10 +124,14 @@ def main():
         #os.system("cls")
         print("0. Exit")
         print("1. Login")
+        print("2. Create Account")
+        print("3.   ")
 
         option = input("Select Menu option: ")
     
         if (option == "0"):
+            print("Thank you for shopping with us")
+            print("Have a Good Day")
             sys.exit()
     
         elif (option == "1"):
@@ -64,6 +151,38 @@ def main():
                     continue
             else:
                 print("Returning to menu.")
+
+        elif (option == "2"):
+
+            username = input("Username: ")
+            user.setusername(username)
+            password = input("Password: ")
+            user.setpassword(password)
+            email = input("Email:")
+            user.setemail(email)
+            firstname = input("First Name: ")
+            user.setfirstname(firstname)
+            lastname = input("Last Name: ")
+            user.setlastname(lastname)
+
+            user.createaccount()
+            store()
+
+        elif (option == "3"):
+            username = "iron"
+            user.setusername(username)
+            password = "pass"
+            user.setpassword(password)
+            email = "email"
+            user.setemail(email)
+            firstname = "gavin"
+            user.setfirstname(firstname)
+            lastname = "eley"
+            user.setlastname(lastname)
+
+            user.createaccount()
+            store()
+
 
 if __name__ == "__main__":
     main()
--- user.py
+++ user.py
@@ -1,4 +1,5 @@
 import mysql.connector
+from mysql.connector import MySQLConnection, Error
 import sys
 
 
@@ -11,38 +12,62 @@ class User:
         self.email = ""
         self.firstname = ""
         self.lastname = ""
-        self.userid = ""
-        
-    
+        self.userid = 0
+        self.shippingid = 0
+        self.card = ""
+        self.cvv = ""
+        self.address = ""
+        self.city = ""
+        self.state = ""
+        self.zipcode = ""
+
     def setusername(self, username):
         self.username = username
+    
+    def getusername(self):
+        return self.username 
 
     def setpassword(self, password):
         self.password = password
     
+    def getpassword(self):
+        return self.password 
+    
     def setemail(self, email):
         self.email = email
-        
+
+    def getemail(self):
+        return self.email 
+
     def setfirstname(self, firstname):
         self.firstname = firstname
         
+    def getfirstname(self):
+        return self.firstname 
+        
     def setlastname(self, lastname): 
         self.lastname = lastname
+      
+    def getlastname(self): 
+        return self.lastname 
+     
+    def getcard(self):
+        return self.card
     
-    def getusername(self, username):
-        self.username = username
+    def getcvv(self):
+        return self.cvv
 
-    def getpassword(self, password):
-        self.password = password
-    
-    def getemail(self, email):
-        self.email = email
-        
-    def getfirstname(self, firstname):
-        self.firstname = firstname
-        
-    def getlastname(self, lastname): 
-        self.lastname = lastname
+    def getaddress(self):
+        return self.address
+
+    def getcity(self):
+        return self.city
+
+    def getstate(self):
+        return self.state
+
+    def getzipcode(self):
+        return self.zipcode
 
     def loginaccount(self):
         try:
@@ -75,7 +100,9 @@ class User:
                 return True
             else:
                 return False
-        
+        cursor.close()
+        connection.close()
+
     def createaccount(self):
         try:
             connection = mysql.connector.connect(
@@ -95,14 +122,188 @@ class User:
         print()
         cursor = connection.cursor()
 
-        query = "INSERT INTO users (firstname, lastname, username, password, email) VALUES (%s,%s,%s,%s,%s,%s)"
-        data = self.firstname + "," + self.lastname + "," + self.username + "," + self.password + "," + self.email
+        cursor.execute("SELECT UserIds, username FROM users")
 
-        cursor.execute(query, data)
+        result = cursor.fetchall()
 
-        connection.commit()
-        print(cursor.rowcount, "record inserted.")
+        numofusers = 0
+        for x in result:
+            numofusers += 1
+            if (x[1] == self.username):
+                return False
+                
+
+
+        query = "INSERT INTO users (UserIDs, firstname, lastname, username, password, email) VALUES(%s, %s, %s, %s, %s, %s)"
+        data = (numofusers, self.firstname, self.lastname, self.username, self.password, self.email)
+        self.userid = numofusers
+        
+        try:
+            cursor.execute(query, data)
+            connection.commit()
+            print(cursor.rowcount, "record inserted.")
+            print()
+        
+        except Error as error:
+            print(error)
+        finally:
+            cursor.close()
+            connection.close()
+       
+    def setshippinginfo(self, address, city, state, zipcode):
+        try:
+            connection = mysql.connector.connect(
+                host="localhost",
+                user="root",
+                password="password",
+                database="projectschema"
+            )
+
+            print("Successful connection.")
+
+        except:
+            print("Failed connection.")
+
+            ## exits the program if unsuccessful
+        
         print()
-        
-        
+        cursor = connection.cursor()
 
+        cursor.execute("SELECT ShippingIDS, UserIDs FROM shipping")
+
+        result = cursor.fetchall()
+
+        numofshipids = 0
+        for x in result:
+            numofshipids += 1
+            if (x[1] == self.userid):
+                return False
+
+        query = "INSERT INTO shipping (ShippingIDs, UserIDs, address, city, state, zipcode) VALUES(%s, %s, %s, %s, %s)"
+        data = (numofshipids, self.userid, address, city, state, zipcode)
+
+        try:
+            cursor.execute(query, data)
+            connection.commit()
+            print(cursor.rowcount, "record inserted.")
+            print()
+        
+        except Error as error:
+            print(error)
+        finally:
+            cursor.close()
+            connection.close()
+
+    def setpaymentinfo(self, card, cvv):
+        try:
+            connection = mysql.connector.connect(
+                host="localhost",
+                user="root",
+                password="password",
+                database="projectschema"
+            )
+
+            print("Successful connection.")
+
+        except:
+            print("Failed connection.")
+
+            ## exits the program if unsuccessful
+        
+        print()
+        cursor = connection.cursor()
+
+        cursor.execute("SELECT paymentID, UserIDs FROM payment")
+
+        result = cursor.fetchall()
+
+        numofpayment = 0
+        for x in result:
+            numofpayment += 1
+            if (x[1] == self.userid):
+                return False
+
+        query = "INSERT INTO shipping (paymentID, UserIDs, card, cvv) VALUES(%s, %s, %s, %s)"
+        data = ( numofpayment, self.userid, card, cvv)
+
+        try:
+            cursor.execute(query, data)
+            connection.commit()
+            print(cursor.rowcount, "record inserted.")
+            print()
+        
+        except Error as error:
+            print(error)
+        finally:
+            cursor.close()
+            connection.close()
+
+    def getpaymentinfo(self):
+        try:
+            connection = mysql.connector.connect(
+                host="localhost",
+                user="root",
+                password="password",
+                database="projectschema"
+            )
+
+            print("Successful connection.")
+
+        except:
+            print("Failed connection.")
+
+            ## exits the program if unsuccessful
+        
+        print()
+        cursor = connection.cursor()
+
+        cursor.execute("SELECT paymentID, UserIDs, card, cvv FROM payment")
+
+        result = cursor.fetchall()
+
+        for x in result:
+            if (x[1] == self.userid):
+                self.card = x[3]
+                self.cvv = x[4]
+                cursor.close()
+                connection.close()
+                return True
+            else:
+                return False
+        
+    def getshippinginfo(self):
+        try:
+            connection = mysql.connector.connect(
+                host="localhost",
+                user="root",
+                password="password",
+                database="projectschema"
+            )
+
+            print("Successful connection.")
+
+        except:
+            print("Failed connection.")
+
+            ## exits the program if unsuccessful
+        
+        print()
+        cursor = connection.cursor()
+
+        cursor.execute("SELECT ShippingIDS, UserIDs, address, city, state, zipcode FROM shipping")
+
+        result = cursor.fetchall()
+
+        for x in result:
+            if (x[1] == self.userid):
+                self.address = x[3]
+                self.city = x[4]
+                self.state = x[5]
+                self.zipcode = x[6]
+                cursor.close()
+                connection.close()
+                return True
+            else:
+                return False
+
+    
