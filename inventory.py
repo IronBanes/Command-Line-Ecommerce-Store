fe0854a1e44a8604c682392c47950ee8e9543174
Caliberj456 <130528251+Caliberj456@users.noreply.github.com>
2023-04-28 19:52:40 -0500

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -1,24 +1,28 @@
 import mysql.connector
 import sys
+import os
 
-class inventory:
-    def _init_ (self):
-        self.itemID = ""
+class Store:
+    Game1 = "Halo 2"
+    Game2 = "Titanfall 2"
+    Game3 = "Super Mario Galaxy"
+    Game4 = "Call of Duty 4"
+    Game5 = "Need For Speed Hot Pursuit"
+    Game6 = "Super Smash Bros Ulitmate"
+    Game7 = "Driver San Francisco"
+    Game8 = "Legend of Zelda: Breath of the Wild"
+    Game9 = "Metroid Prime"
+    Game10 = "Sonic Frontiers"
 
-    def viewGameInventory():
-        storeloop = 0
-        while (storeloop != 0):
-            os.system("cls")
+    print(Game1, "\n", Game2, "\n", Game3, "\n", Game4, "\n", Game5)
+    print(Game6, "\n", Game7, "\n", Game8, "\n", Game9, "\n", Game10)
 
-            print("0. back")
-            print("1. View Games")
-            print("2. Cart")
+   
 
-            if(storeloop == "0"):
 
-            elif(storeloop = "1"):
 
-            elif(storeloop = "2"):
-                print("Cart")
 
+
+    
+        
 
