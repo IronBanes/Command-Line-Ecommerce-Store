1c12d8cc527ffe0de7136e381c7710d1037a0456
Caliberj456 <130528251+Caliberj456@users.noreply.github.com>
2023-04-29 17:09:39 -0500

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -22,15 +22,15 @@ class Inventory:
     cursor = connection.cursor()
 
     query = "INSERT INTO inventory (ItemID, stockcount, title, genre, publisher, price) VALUES (%s,%s,%s,%s,%s,%s,%s)"
-    data1 = 1123 + "," + 14 + "," + Game1 + "," + "Shooter" + "," + "Microsoft Game Studios" + 6.99
-    data2 = 1223 + "," + 10 + "," + Game2 + "," + "Shooter" + "," + "Electronic Arts" + 6.99
-    data3 = 1323 + "," + 5 + "," + Game3 + "," + "Platformer" + "," + "Nintendo" + 10.99
-    data4 = 1423 + "," + 2 + "," + Game4 + "," + "Shooter" + "," + "Activision" + 4.99
-    data5 = 1523 + "," + 20 + "," + Game5 + "," + "Racing" + "," + "Electronic Arts" + 19.99
-    data6 = 1623 + "," + 15 + "," + Game6 + "," + "Fighting" + "," + "Nintendo" + 59.99
-    data7 = 1723 + "," + 10 + "," + Game7 + "," + "Racing" + "," + "Ubisoft" + 9.99
-    data8 = 1823 + "," + 15 + "," + Game8 + "," + "Adventure" + "," + "Nintendo" + 59.99
-    data9 = 1923 + "," + 20 + "," + Game9 + "," + "Adventure" + "," + "Sega" + 59.99
+    data1 = "1123" + "," + "14" + "," + Game1 + "," + "Shooter" + "," + "Microsoft Game Studios" + "6.99"
+    data2 = "1223" + "," + "10" + "," + Game2 + "," + "Shooter" + "," + "Electronic Arts" + "6.99"
+    data3 = "1323" + "," + "5" + "," + Game3 + "," + "Platformer" + "," + "Nintendo" + "10.99"
+    data4 = "1423" + "," + "2" + "," + Game4 + "," + "Shooter" + "," + "Activision" + "4.99"
+    data5 = "1523" + "," + "20" + "," + Game5 + "," + "Racing" + "," + "Electronic Arts" + "19.99"
+    data6 = "1623" + "," + "15" + "," + Game6 + "," + "Fighting" + "," + "Nintendo" + "59.99"
+    data7 = "1723" + "," + "10" + "," + Game7 + "," + "Racing" + "," + "Ubisoft" + "9.99"
+    data8 = "1823" + "," + "15" + "," + Game8 + "," + "Adventure" + "," + "Nintendo" + "59.99"
+    data9 = "1923" + "," + "20" + "," + Game9 + "," + "Adventure" + "," + "Sega" + "59.99"
     
     cursor.execute(query,data1)
     cursor.execute(query,data2)
