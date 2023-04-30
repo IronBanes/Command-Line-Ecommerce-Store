64431b793602c332cb3b967436ca2870a5bcf9a5
Caliberj456 <130528251+Caliberj456@users.noreply.github.com>
2023-04-29 21:04:54 -0500

    Change 4

--- inventory.py
+++ inventory.py
@@ -2,52 +2,71 @@ import mysql.connector
 import sys
 import os    
 class Inventory:
-    Game1 = "Halo 2"
-    Game2 = "Titanfall 2"
-    Game3 = "Super Mario Galaxy"
-    Game4 = "Call of Duty 4"
-    Game5 = "Need For Speed Hot Pursuit Remasterd"
-    Game6 = "Super Smash Bros Ulitmate"
-    Game7 = "Driver San Francisco"
-    Game8 = "Legend of Zelda: Breath of the Wild"
-    Game9 = "Sonic Frontiers"
+    def __init__(self):
+        self.itemID = ""
+        self.stock = ""
+        self.title = ""
+        self.genre = ""
+        self.publisher = ""
+        self.price = ""
 
-    connection = mysql.connector.connect(
-        host= "localhost",
-        user= "root",
-        passwd= "password",
-        database= "projectschema",
-    )
+        
+    def setitemID(self, itemID):
+        self.itemID = itemID
     
-    cursor = connection.cursor()
+    def setstock(self, stock):
+        self.stock = stock
 
-    query = "INSERT INTO inventory (ItemID, stockcount, title, genre, publisher, price) VALUES (%s,%s,%s,%s,%s,%s,%s)"
-    data1 = 1123 + "," + 14 + "," + Game1 + "," + "Shooter" + "," + "Microsoft Game Studios" + 6.99
-    data2 = 1223 + "," + 10 + "," + Game2 + "," + "Shooter" + "," + "Electronic Arts" + 6.99
-    data3 = 1323 + "," + 5 + "," + Game3 + "," + "Platformer" + "," + "Nintendo" + 10.99
-    data4 = 1423 + "," + 2 + "," + Game4 + "," + "Shooter" + "," + "Activision" + 4.99
-    data5 = 1523 + "," + 20 + "," + Game5 + "," + "Racing" + "," + "Electronic Arts" + 19.99
-    data6 = 1623 + "," + 15 + "," + Game6 + "," + "Fighting" + "," + "Nintendo" + 59.99
-    data7 = 1723 + "," + 10 + "," + Game7 + "," + "Racing" + "," + "Ubisoft" + 9.99
-    data8 = 1823 + "," + 15 + "," + Game8 + "," + "Adventure" + "," + "Nintendo" + 59.99
-    data9 = 1923 + "," + 20 + "," + Game9 + "," + "Adventure" + "," + "Sega" + 59.99
+    def settitle(self, title):
+        self.title = title
     
-    cursor.execute(query,data1)
-    cursor.execute(query,data2)
-    cursor.execute(query,data3)
-    cursor.execute(query,data4)
-    cursor.execute(query,data5)
-    cursor.execute(query,data6)
-    cursor.execute(query,data7)
-    cursor.execute(query,data8)
-    cursor.execute(query,data9)
+    def setgenre(self, genre):
+        self.genre = genre
 
+    def setpublisher(self, publisher):
+        self.publisher = publisher
+
+    def setprice(self, price):
+        self.price = price
+ 
+   
+   
+    def getitemID(self, itemID):
+        self.itemID = itemID
     
-    connection.commit()
+    def getstock(self, stock):
+        self.stock = stock
 
-    result = cursor.fetchall()
+    def gettitle(self, title):
+        self.title = title
+    
+    def getgenre(self, genre):
+        self.genre = genre
 
-    print(result)
+    def getpublisher(self, publisher):
+        self.publisher = publisher
+
+    def getprice(self, price):
+        self.price = price
+
+
+    def items(self):
+
+        connection = mysql.connector.connect(
+                host="localhost",
+                user="root",
+                password="password",
+                database="projectschema"
+            )
+        
+        cursor = connection.cursor()
+
+        cursor.execute("SELECT * FROM inventory")
+
+        result = cursor.fetchall()
+
+        for x in result:
+            print("Entire Row:",x,"\n")
 
 
 
